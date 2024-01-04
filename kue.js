2a5eac673bbf0f47b13c232948a08d08861e178a
robybet <155623772+robybet@users.noreply.github.com>
2024-01-04 22:37:37 +0700

    first commit

---
+++ kue.js
@@ -0,0 +1,15 @@
+function showGift() {
+    document.getElementById('giftModal').style.display = 'block';
+}
+
+function showCake() {
+    alert('Selamat Ulang Tahun! 🎂');
+}
+
+function showBalloon() {
+    alert('Happy Birthday! 🎈');
+}
+
+function closeModal() {
+    document.getElementById('giftModal').style.display = 'none';
+}
